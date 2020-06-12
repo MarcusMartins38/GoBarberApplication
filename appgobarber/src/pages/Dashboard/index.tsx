2f81439902ea60db818e6ba7ba2532cff86eb74e
Marcus Martins <marcusmartins38@gmail.com>
2020-06-12 18:28:40 -0400

    Images for Readme GitHub

--- appgobarber/src/pages/Dashboard/index.tsx
+++ appgobarber/src/pages/Dashboard/index.tsx
@@ -39,7 +39,7 @@ const Dashboard: React.FC = () => {
     api.get('providers').then((response) => {
       setProviders(response.data);
     });
-  }, []);
+  }, [providers]);
 
   const navigateToProfile = useCallback(() => {
     navigate('Profile');
