55a1a0fe16b1e12c9420db4e58993ef3f3647a0c
Marcus Martins <marcusmartins38@gmail.com>
2020-06-21 15:01:28 -0400

    Test Hook: Auth

--- frontend/src/hooks/auth.tsx
+++ frontend/src/hooks/auth.tsx
@@ -88,8 +88,5 @@ export const AuthProvider: React.FC = ({ children }) => {
 export function useAuth(): AuthContextData {
   const context = useContext(AuthContext);
 
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
   return context;
 }
